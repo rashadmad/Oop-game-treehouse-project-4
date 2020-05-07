133fa9daef27c816c91d96be82290066a2e5f3e4
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-05-06 17:23:55 -0700

    psuedo code prep

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,5 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ //to create a Game class with methods for starting and ending the game, handling interactions, getting a random phrase, checking for a win, and removing a life from the scoreboard.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ //create a new instance of the Game class and add event listeners for the start button and onscreen keyboard buttons.
