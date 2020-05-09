87b38faec1c752cec7b604f926ddb4eda820f14d
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-05-09 16:00:36 -0700

    add a function to handle empty box creation

--- js/HelperFunctions.js
+++ js/HelperFunctions.js
@@ -1,2 +1,13 @@
 //generates a random number
 const generateRandomNumber = (max,min) => Math.floor(Math.random() * (+max - +min)) + +min;    
+//create a function for filltering outpunctuatiton
+const phraseFillteredOfPunctuation = (stringToBeFilltered) => this.phrase.replace(/[.,\/#!$%.?\^&\*;:{}=\-_`~()]/g,"")
+// Each letter is presented by an empty box, one li element for each letter. 
+const emptyLetterBox = (letter) => {
+    const emptyBox = document.createElement('li');
+    emptyBox.classList += "hide" + " " + "letter"
+    emptyBox.createTextNode = letter;
+    phraseContainer.appendChild(letter); 
+} 
+
+
--- js/Phrase.js
+++ js/Phrase.js
@@ -9,10 +9,9 @@ class Phrase {
     }
 //The phrase class should have these three methods
    addPhraseToDisplay(){
-    // Need to fillter out any charicters that arent spaces or letters
-    let phraseFillteredOfPunctuation = this.phrase.replace(/[.,\/#!$%.?\^&\*;:{}=\-_`~()]/g,"")
+
     // this adds letter placeholders to the display when the game starts. 
-    // Each letter is presented by an empty box, one li element for each letter. 
+    
     // See the example_phrase_html.txt file for an example of what the rendered HTML for a phrase should look like when the game starts, including any id or class attributes needed. 
     // When the player correctly guesses a letter, the empty box is replaced with the matched letter (see the showMatchedLetter() method below). 
     // Make sure the phrase displayed on the screen uses the letter CSS class for letters and the space CSS class for spaces. 
